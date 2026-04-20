3a7a2f250df154093200bc002b66861abf512f67
danielahernav2006 <danihena2006@gmail.com>
2026-04-19 21:18:07 -0500

    Corrige lint de api.py

--- app/api.py
+++ app/api.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 import joblib
-import numpy as np
 import pandas as pd
 
 model = joblib.load("app/model.joblib")
 app = FastAPI()
+
 
 class Input(BaseModel):
     Age: int
@@ -13,12 +13,13 @@ class Input(BaseModel):
     ChestPainType: str
     RestingBP: int
     Cholesterol: int
-    FastingBS: str    
+    FastingBS: str
     RestingECG: str
     MaxHR: int
     ExerciseAngina: str
     Oldpeak: float
     ST_Slope: str
+
 
 @app.post("/predict")
 def predict(data: Input):
@@ -27,7 +28,7 @@ def predict(data: Input):
         proba = model.predict_proba(X)[0][1]
         return {
             "heart_disease_probability": round(float(proba), 4),
-            "prediction": int(proba > 0.5)
+            "prediction": int(proba > 0.5),
         }
     except Exception as e:
         return {"error": str(e)}
